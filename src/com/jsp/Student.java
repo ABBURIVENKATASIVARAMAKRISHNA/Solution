24dcc52742d0884f4ee8e1949033ef63d8709faf
ABBURIVENKATASIVARAMAKRISHNA <avsrk2002@gmial.com>
2023-12-14 22:08:03 +0530

    initaited

---
+++ src/com/jsp/Student.java
@@ -0,0 +1,26 @@
+package com.jsp;
+
+public class Student {
+
+	int id;
+	String name;
+	long contact;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getContact() {
+		return contact;
+	}
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+}
